2c2c7eb5269c85cd70de1c8ac45ff21aeeb5535b
nikraev <nikraev@yandex.ru>
2014-12-16 18:28:57 +0300

    Add some str to qsort

--- sort/sort.c
+++ sort/sort.c
@@ -3,7 +3,14 @@
 
 #define MAXLINES 5000
 
+char* lineptr[MAXLINES];
+
+int readlines(char* lineptr[], int nlines);
+void wntelines(char* lineptr[],int nlines);
+void qsort(char* lineptr[],int left,int right);
+
 void main(void)
 {
+  int nlines;
 
 }
