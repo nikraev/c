304e204f3783b1c09fea1063e7a59201a37aaf80
nikraev <nikraev@yandex.ru>
2014-12-16 18:20:06 +0300

    Add some files

---
+++ sort/sort.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000
+
+void main(void)
+{
+
+}
--- strend/strend.c
+++ strend/strend.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
+#include <string.h>
 
 int strend(char* a, char* b);
+int strendv2(char* a, char* b);
 
 void main(void)
 {
   char* a = "hello";
-  char* b = "llo";
+  char* b = "he1llo";
 
   if(strend(a,b) == 1)
     {
-      printf("Оканчивается");
+      printf("Оканчивается\n");
     }
   else
     {
-      printf("Не оканчивается");
+      printf("Не оканчивается\n");
+    }
+  
+  if(strendv2(a,b) == 1)
+    {
+      printf("Оканчивается\n");
+    }
+  else
+    {
+      printf("Не оканчивается\n");
     }
 }
+
+int strend(char* a, char* b)
+{
+  int n = strlen(b);
+  a+=strlen(a);
+  b+=n;
+  
+  for(int i = 0 ; i < n; i++, a--, b--)
+  {
+     if(*a != *b) {return 0;}
+  }
+
+  return 1;
+}
+
+
